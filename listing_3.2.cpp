c70997e5acbe807e8a8d465909be153c626578a1
tangxiang <tangx@amyrobotics.com>
2019-06-14 18:30:29 +0800

    add listing_3.2.cpp - listing_3.5.cpp; update README.md

---
+++ listing_3.2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+#include <algorithm>
+#include <list>
+#include <mutex>
+
+class some_data
+{
+	int a;
+	std::string b;
+public:
+	void do_somthing() {}
+};
+
+class data_wrapper
+{
+	some_data data;
+	std::mutex m;
+public:
+	template<typename Function>
+	void process_data(Function func)
+	{
+		std::lock_guard<std::mutex> l(m);
+		func(data);
+	}
+	
+};
+
+some_data *unprotected;
+
+void malicious_function(some_data &protected_data)
+{
+	unprotected = &protected_data;
+}
+
+data_wrapper x;
+void foo()
+{
+	x.process_data(malicious_function);
+	unprotected->do_somthing();
+}
+
+int main()
+{
+	foo();
+
+	return 0;
+}
---
+++ listing_3.5.cpp
@@ -0,0 +1,26 @@
+#include <exception>
+#include <memory>
+
+struct empty_stack : std::exception
+{
+    const char* what() const throw();
+};
+
+template<typename T>
+class threadsafe_stack
+{
+public:
+    threadsafe_stack();
+    threadsafe_stack(const threadsafe_stack&);
+    threadsafe_stack& operator=(const threadsafe_stack&) = delete;
+
+    void push(T new_value);
+    std::shared_ptr<T> pop();
+    void pop(T& value);
+    bool empty() const;
+};
+
+int main()
+{
+    return 0;
+}

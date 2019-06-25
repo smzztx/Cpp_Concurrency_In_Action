8341bf45632bd1d77b3719a6f4e1505711b72150
tangxiang <tangxiang007@gmail.com>
2019-06-25 08:23:09 -0700

    add listing_4.15.cpp - listing_4.16.cpp; update README.md

---
+++ listing_4.15.cpp
@@ -0,0 +1,45 @@
+struct card_insert
+{
+	std::string account;
+};
+
+class atm
+{
+private:
+	messaging::receiver incoming;
+	messaging::sender bank;
+	messaging::sender interface_hardware;
+	void (atm::*state)();
+
+	std::string account;
+	std::string pin;
+
+	void waiting_for_card()
+	{
+		interface_hardware.send(display_enter_card());
+		incoming.wait().handle<card_inserted>(
+			[&](card_inserted const &msg)
+			{
+				account = msg.account;
+				pin = "";
+				interface_hardware.send(display_enter_card());
+				state = &atm::getting_pin;
+			}
+		);
+	}
+	void getting_pin();
+public:
+	void run()
+	{
+		state = &atm::waiting_for_card;
+		try{
+			for(;;)
+			{
+				(this->*state)();
+			}
+		}catch(messaging::close_queue const&)
+		{
+
+		}
+	}
+};
